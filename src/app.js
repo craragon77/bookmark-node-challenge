12d4eeaf826136a8b6d9afaeee16670b465b0d3f
Chris Aragon <aragonchristopherray@gmail.com>
2020-05-05 16:45:59 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -4,12 +4,49 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const winston = require('winston');
 
 const app = express()
+console.log(process.env.API_TOKEN)
+const bookmark = [{
+    id: 1,
+    title: 'Appalachian Trail Website',
+    url: 'https://appalachiantrail.org/'
+}, {
+    id: 2,
+    title: 'Pacific Crest Trail website',
+    url: 'https://www.pcta.org/'
+}]
+
+app.use(function validateBearerToken(req,res,next){
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    debugger
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({filename: 'info.log'})
+    ]
+})
+
+if (NODE_ENV !== 'production'){
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }))
+}
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
-  : 'common';
+  : 'dev';
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
